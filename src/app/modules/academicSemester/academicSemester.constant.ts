61f5a610f2de094196b929770369c14951d7e75a
rakibulhasan044 <rhrakib044@gmail.com>
2024-12-01 04:01:55 +0600

    added valid academic year checker function

--- src/app/modules/academicSemester/academicSemester.constant.ts
+++ src/app/modules/academicSemester/academicSemester.constant.ts
@@ -1,4 +1,4 @@
-import { TAcademicSemesterCode, TAcademicSemesterName, TMonths } from "./academicSemester.interface";
+import { TAcademicSemesterCode, TAcademicSemesterCodeMapper, TAcademicSemesterName, TMonths } from "./academicSemester.interface";
 
 export const Months: TMonths[] = [
     'January',
@@ -16,4 +16,11 @@ export const Months: TMonths[] = [
   ];
   
   export const AcademicSemesterName: TAcademicSemesterName[] = ['Autumn' , 'Summer', 'Fall'];
+
   export const AcademicSemesterCode: TAcademicSemesterCode[] = ['01', '02', '03']
+
+  export const academicSemesterCodeMapper: TAcademicSemesterCodeMapper = {
+    Autumn: '01',
+    Summer: '02',
+    Fall: '03'
+}
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,7 +1,14 @@
+import { academicSemesterCodeMapper } from "./academicSemester.constant";
 import { TAcademicSemester } from "./academicSemester.interface";
 import { AcademicSemester } from "./academicSemester.model";
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+
+    //semester name -> semester code validation
+
+    if(academicSemesterCodeMapper[payload.name] !== payload.code) {
+        throw new Error("Invalid semester code !!")
+    }
     const result = await AcademicSemester.create(payload);
 
     return result;
