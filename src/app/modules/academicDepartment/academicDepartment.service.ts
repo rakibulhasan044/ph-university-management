e40260d2732bc04c2bff7ae2ae0a2f4ef3591adf
rakibulhasan044 <rhrakib044@gmail.com>
2024-12-06 04:15:01 +0600

    create and update validation updated

--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -2,6 +2,7 @@ import { TAcademicDepartment } from "./academicDepartment.interface";
 import { AcademicDepartment } from "./academicDepartment.model";
 
 const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
+
     const result = await AcademicDepartment.create(payload);
     return result;
 }
