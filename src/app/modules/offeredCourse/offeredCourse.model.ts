0e8b6a91c78ea0dd9760ff97cefd49d02f6cb9ed
rakibulhasan044 <rhrakib044@gmail.com>
2024-12-19 16:03:12 +0600

    added offered course interface and model

---
+++ src/app/modules/offeredCourse/offeredCourse.model.ts
@@ -0,0 +1,60 @@
+import mongoose, { Schema } from "mongoose";
+import { TOfferedCourse } from "./offeredCourse.routes";
+import { Days } from "./offeredCourse.constant";
+
+const offeredCourseSchema = new mongoose.Schema<TOfferedCourse>({
+    semesterRegistration: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'SemesterRegistration'
+    },
+    academicSemester: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'AcademicSemester'
+    },
+    academicFaculty: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'AcademicFaculty'
+    },
+    academicDepartment: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'AcademicDepartment'
+    },
+    course: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Course'
+    },
+    faculty: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Faculty'
+    },
+    maxCapacity: {
+        type: Number,
+        required: true
+    },
+    section: {
+        type: Number,
+        required: true,
+    },
+    days: {
+        type: String,
+        enum: Days
+    },
+    startTime: {
+        type: String,
+        required: true
+    },
+    endTime: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+export const OfferedCourse = mongoose.model<TOfferedCourse>('offeredCourse', offeredCourseSchema)
