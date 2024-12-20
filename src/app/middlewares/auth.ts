08d9ac4abda4d7ccae7a79ce50f81fbed516fd57
rakibulhasan044 <rhrakib044@gmail.com>
2024-12-21 01:54:51 +0600

    added user from jwt to global express request

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
+
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
@@ -8,7 +11,19 @@ const auth = () => {
     if (!token) {
       throw new AppError(401, 'Unauthorized request');
     }
-    next();
+
+    //check if the token is valid
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        if (err) {
+          throw new AppError(401, 'You are not authorized');
+        }
+        req.user = decoded as JwtPayload;
+        next();
+      },
+    );
   });
 };
 
